15f0ad500165556aad5c9e01ae463f9f3388f2e2
Vitor Rodrigo Rosan <vitor.rosan@serpro.gov.br>
2017-04-18 11:43:23 -0300

    Inclusão de atributo para inibir ou habilitar a porção do DOM caso o usuário não esteja logado. Também há uma otimização para evitar ficar removendo e incluindo a porção do DOM a cada passada

--- modules/@demoiselle/security/src/dml-is-logged.directive.ts
+++ modules/@demoiselle/security/src/dml-is-logged.directive.ts
@@ -8,19 +8,24 @@ import { Subscription } from 'rxjs/Subscription';
  *
  * ```
  * <div *dmlIsLogged>
- *    Somente usuário logado pode ver esse conteúdo!!!
+ *  Somente usuário logado pode ver esse conteúdo!!!
+ * </div>
+ * <div *dmlIsLogged="false">
+ *  Somente se não estiver logado pode ver esse conteúdo!!!
  * </div>
  * ```
  *
  * ### Syntax
  *
- * `<div *dmlIsLogged>...</div>`
+ * `<div *dmlIsLogged[="true|false"]>...</div>`
  *
  */
 @Directive({
   selector: '[dmlIsLogged]'
 })
 export class DmlIsLoggedDirective {
+  private enabled: boolean = true; // indica se é para mostrar caso esteja logado (true) ou não logado (false)
+  private hasView: boolean = false; // indica se a porção do DOM está visível ou não
 
   loginSubscription: Subscription;
 	
@@ -36,14 +41,27 @@ export class DmlIsLoggedDirective {
 
 
   @Input()
-  set dmlIsLogged(empty: any) {
+  set dmlIsLogged( enabled: boolean ) {
+    // se o atributo enabled não for definido então define o valor padrão como true, ou seja, exibe se estiver logado
+    this.enabled = ( enabled === null || enabled === undefined ) || enabled;
     this.updateView();
   }
 
-  private updateView(){
-    this._viewContainer.clear();
-    if (this.authService.isAuthenticated()) {
-      this._viewContainer.createEmbeddedView(this._template);
+  private updateView() {
+    if ( ( ( !this.authService.isAuthenticated() ) ? this.enabled : !this.enabled ) && this.hasView ) {
+      /* se a porção do DOM estiver visível então esconde se
+       * 1) não estiver autenticado e enabled=true
+       * 2) estiver autenticado e enabled=false
+       */
+      this._viewContainer.clear();
+      this.hasView = false;
+    } else if ( ( this.authService.isAuthenticated() ? this.enabled : !this.enabled ) && !this.hasView ) {
+      /* se a porção do DOM não estiver visível, então torna visível se
+       * 1) estiver autenticado e enabled=true
+       * 2) não estiver autenticado e enabled=false
+       */
+      this._viewContainer.createEmbeddedView( this._template );
+      this.hasView = true;
     }
   }
 
